d31d8c2984412b7da5cbb44a3b9cbe806d672b23
deebferreira <debfpsnt@gmail.com>
2026-08-20 22:42:19 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/qapipeline/QaDevopsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.qapipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaDevopsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaDevopsPipelineApplication.class, args);
+	}
+
+}
