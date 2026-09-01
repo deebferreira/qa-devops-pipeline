005f38ebd69a057f110198651375001946083d0f
Daniele Mortati <mortatidani@gmail.com>
2026-08-31 22:02:53 -0300

    test: fix expected status for nonexistent task

--- src/test/java/com/qapipeline/api/TaskGetByIdApiTest.java
+++ src/test/java/com/qapipeline/api/TaskGetByIdApiTest.java
@@ -108,6 +108,6 @@ class TaskGetByIdApiTest {
                 .when()
                 .request("GET", "/tasks/" + nonexistentId);
 
-        assertEquals(200, response.statusCode());
+        assertEquals(404, response.statusCode());
     }
 }
